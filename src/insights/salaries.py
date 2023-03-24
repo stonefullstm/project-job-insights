53c742b1d66a68c9a2c3db3e7f46d0bc8bea46e1
stonefullstm <stonefull.stm@gmail.com>
2023-03-24 16:19:23 -0300

    feat: Requisito 4 - implementação da função get_max_salary

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -1,22 +1,16 @@
 from typing import Union, List, Dict
+from src.insights.jobs import read
 
 
 def get_max_salary(path: str) -> int:
-    """Get the maximum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The maximum salary paid out of all job opportunities
-    """
-    raise NotImplementedError
+    jobs_data = read(path)
+    max_salary = 0
+    for job in jobs_data:
+        salary = job['max_salary']
+        if salary != '' and not salary.isalpha():
+            if int(salary) > max_salary:
+                max_salary = int(salary)
+    return max_salary
 
 
 def get_min_salary(path: str) -> int:
