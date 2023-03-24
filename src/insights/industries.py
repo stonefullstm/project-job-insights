f37b371944fa0aec9d77fa609bcf586183545352
stonefullstm <stonefull.stm@gmail.com>
2023-03-24 15:44:53 -0300

    feat: Requisito 2 - implementação da função get_unique_industries

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,22 +1,15 @@
 from typing import List, Dict
 
+from jobs import read
+
 
 def get_unique_industries(path: str) -> List[str]:
-    """Checks all different industries and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    raise NotImplementedError
+    jobs_data = read(path)
+    unique_industries = set()
+    for job in jobs_data:
+        if job['industry'] != '':
+            unique_industries.add(job['industry'])
+    return unique_industries
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
