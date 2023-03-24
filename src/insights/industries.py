93152bcfd10119917b604d1e737c68e2272c078a
stonefullstm <stonefull.stm@gmail.com>
2023-03-24 18:11:58 -0300

    feat: Requisito 7 - implementação da função filter_by_industry

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -4,12 +4,13 @@ from src.insights.jobs import read
 
 
 def get_unique_industries(path: str) -> List[str]:
-    jobs_data = read(path)
-    unique_industries = set()
-    for job in jobs_data:
-        if job['industry'] != '':
-            unique_industries.add(job['industry'])
-    return unique_industries
+    jobs = read(path)
+    # unique_industries = set()
+    # for job in jobs_data:
+    #     if job['industry'] != '':
+    #         unique_industries.add(job['industry'])
+    industries = {job['industry'] for job in jobs if job['industry'] != ''}
+    return industries
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
