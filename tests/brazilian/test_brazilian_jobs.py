724fd15da0e02f1cb7c9ce5d8f2ce927cccdafa6
stonefullstm <stonefull.stm@gmail.com>
2023-04-03 15:54:47 -0300

    feat: Requisito 11 - Implementação de teste para a função read_brazilian_file

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,10 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    jobs = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
+    keys_dict = {"titulo": "title", "salario": "salary", "tipo": "type"}
+
+    for key in keys_dict:
+        assert key not in jobs[0]
+        assert keys_dict[key] in jobs[0]
