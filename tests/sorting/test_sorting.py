ab39efff43e527b0982b0ad8fea1aaf1cf6b344d
stonefullstm <stonefull.stm@gmail.com>
2023-04-03 18:52:47 -0300

    feat: Requisito 12 - Implementação de teste para a função sort_by

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,15 @@
-# from src.pre_built.sorting import sort_by
+from src.pre_built.sorting import sort_by
+from tests.sorting.testdata import jobs
+from tests.sorting.testdata import jobs_by_min_salary
+from tests.sorting.testdata import jobs_by_date_posted, jobs_by_max_salary
 
 
 def test_sort_by_criteria():
-    pass
+    sort_by(jobs, "max_salary")
+    assert jobs == jobs_by_max_salary
+
+    sort_by(jobs, "min_salary")
+    assert jobs == jobs_by_min_salary
+
+    sort_by(jobs, "date_posted")
+    assert jobs == jobs_by_date_posted
---
+++ tests/sorting/testdata.py
@@ -0,0 +1,23 @@
+jobs = [
+    {"max_salary": 2500, "min_salary": 2000, "date_posted": "2021-01-13"},
+    {"max_salary": 3000, "min_salary": 0, "date_posted": "2023-10-31"},
+    {"max_salary": 5500, "min_salary": 3000, "date_posted": "2022-02-15"},
+]
+
+jobs_by_max_salary = [
+    {"max_salary": 5500, "min_salary": 3000, "date_posted": "2022-02-15"},
+    {"max_salary": 3000, "min_salary": 0, "date_posted": "2023-10-31"},
+    {"max_salary": 2500, "min_salary": 2000, "date_posted": "2021-01-13"},
+]
+
+jobs_by_min_salary = [
+    {"max_salary": 3000, "min_salary": 0, "date_posted": "2023-10-31"},
+    {"max_salary": 2500, "min_salary": 2000, "date_posted": "2021-01-13"},
+    {"max_salary": 5500, "min_salary": 3000, "date_posted": "2022-02-15"},
+]
+
+jobs_by_date_posted = [
+    {"max_salary": 3000, "min_salary": 0, "date_posted": "2023-10-31"},
+    {"max_salary": 5500, "min_salary": 3000, "date_posted": "2022-02-15"},
+    {"max_salary": 2500, "min_salary": 2000, "date_posted": "2021-01-13"},
+]
